b477126a3de3cf436a1a60ec28d0f674b5b372ff
SaurokiFestus <sauroki.festus@student.moringaschool.com>
2022-10-10 09:39:58 +0300

    Done.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,8 @@
 class ItemsController < ApplicationController
+
     def index
         items = Item.all
-        render json: items, except: [:created_at, :updated_at, :user_id], include: :user, except: [:created_at, :updated_at]
+        render json: items, except: [:created_at, :updated_at, :user_id], 
+        include: {user: {except: [:created_at, :updated_at]}}
     end
 end
